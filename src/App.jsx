4237d7b2eaa1270be9230bfdc01f013e17eeb187
“Dale” <daled2023@icloud.com>
2025-10-06 03:38:49 +0100

    Fix double scroll: Remove overflow-hidden from parent container
    - Removed overflow-hidden from flex-1 flex flex-col container
    - This allows natural scrolling behavior without nested scroll containers
    - Fixes the issue where users couldn't scroll down to see all modules

--- src/App.jsx
+++ src/App.jsx
@@ -38,7 +38,7 @@ function App() {
     <Router future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
       <div className="flex h-screen bg-gray-200">
         <Sidebar />
-        <div className="flex-1 flex flex-col overflow-hidden">
+        <div className="flex-1 flex flex-col">
           <Header />
           <main className="flex-1 overflow-y-auto">
             <AnimatePresence mode="wait">
